b986228ecc706dafdda958a43f31cebc09331743
jasnoor1046 <jasnoor00070@gmail.com>
2024-10-30 17:47:46 +0530

    Fixed the logic in this code

--- odd_string.py
+++ odd_string.py
@@ -1,11 +1,11 @@
-# let us take a string
-string= "HelloWorld"
+# let us take a string and initialize it.
+string= "Hello World"
 
 odd_chars= "" # initializing an empty string to store extracted odd characters.
 
 for i in range(len(string)): # inside this loop it will check if the current index is odd,
 # if the index is odd it will append the character at that index to odd_chars    
-    if i % 2 == 1:
+    if i % 2 != 0:
         odd_chars += string[i] 
         # string[i] is the current character we are looking at 
         # odd_chars += take the current character and add it to the end of the odd_chars.
