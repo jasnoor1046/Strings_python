1bdfd4822aa2a81582e5f43a0756000d83bc3e27
jasnoor1046 <jasnoor00070@gmail.com>
2024-12-26 15:38:04 +0530

    fixed the error

--- reverse_alternate.py
+++ reverse_alternate.py
@@ -1,17 +1,18 @@
-s= input("Enter a string: ")
-# i take a string "How are you"
+s= input("Enter a string: ") #input of the string
 
-words= s.split()
+words= s.split() # it splits the input string
+split_length = len(words) # calculates the length of the words list
 
-reversed_words= []
-for i, word in enumerate(words):  # this function provides the index(i) and the word itself (word) in each iteration
-    
-    if i%2==0:
+result_words= [] # initialized an empty list to store the processed words
 
-        reversed_words.append(word[::-1]) # reverses the word then appends
+for i in range(split_length):
+    if i % 2 == 1: # it checks if the index is odd
+        result_words.append(words[i] [::-1]) # reverses the word at that index, appends the reversed words to result_words
     else:
-        reversed_words.append(word) # appends the word without reversing
+        result_words.append(words[i]) # appends the words as it is
 
-        reverse= ' '.join(reversed_words)
-        print("After reversing string of alternative word:")
-        print(reverse)
+        #joining the words into a single string
+        result= ' '.join(result_words)
+
+        #printing the result
+        print(result)        
