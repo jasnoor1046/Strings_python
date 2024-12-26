7412e2a295dc210626d0f5b8e3b00358fd69fe65
jasnoor1046 <jasnoor00070@gmail.com>
2024-12-26 18:17:00 +0530

    fixed the code

--- reverse_alternate.py
+++ reverse_alternate.py
@@ -1,18 +1,27 @@
-s= input("Enter a string: ") #input of the string
+# funtion here is initialized to reverse the alternative words
+def reverse_alternate_words(sentence):
 
-words= s.split() # it splits the input string
-split_length = len(words) # calculates the length of the words list
+    # in the particular string it will split the words
+    words = sentence.split()
 
-result_words= [] # initialized an empty list to store the processed words
+    # a separate list has also been intialized for it
+    result = []
 
-for i in range(split_length):
-    if i % 2 == 1: # it checks if the index is odd
-        result_words.append(words[i] [::-1]) # reverses the word at that index, appends the reversed words to result_words
-    else:
-        result_words.append(words[i]) # appends the words as it is
+    # this will itterate over the words of sentence for odd indices
+    for i in range(len(words)):
+        if i % 2 == 1:
 
-        #joining the words into a single string
-        result= ' '.join(result_words)
+            # if it get odd index then it will reverse that word then add to new list
+            result.append(words[i][::-1])
+        else:
 
-        #printing the result
-        print(result)        
+            # if its even then it will remain unchanged
+            result.append(words[i])
+
+            # join the words in the new list
+    return ' '.join(result)
+
+sentence = input("Enter a sentence: ")
+result = reverse_alternate_words(sentence)
+print(result)
+
