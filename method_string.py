d4da7b6197b4a04c3139d66007fdc645e5e10bf7
jasnoor1046 <jasnoor00070@gmail.com>
2024-12-29 20:37:50 +0530

    applied various methods to the string

---
+++ method_string.py
@@ -0,0 +1,27 @@
+name= "jasnoor"
+
+age= "18"
+
+print(len(name)) # prints the length of the string
+
+print(name.endswith("noor")) # whether the string ends with the letters given in bracket
+
+print(name.startswith("a")) # whether the string starts with the letters given in bracket
+
+print(name.count("j")) # to count how many times this letter occurs
+
+print(name.find("noor")) # to find something
+
+print(name.replace("noor", "noorie")) # to replace a part of string with some other word
+
+print(name.capitalize()) # to print first letter in upper case
+
+print(name.upper()) # all lettters will be printed in upper case
+
+print(name.lower()) # all letters will be printed in lower case
+
+print(name.split()) # to split the string
+
+message= f"Hello,{name}! you are {age} years old."
+print(message)
+# using f- strings method for concatenation
