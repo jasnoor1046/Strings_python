6ec6c53d18a6ebdcd28c32400782f14222617fe3
jasnoor1046 <jasnoor00070@gmail.com>
2025-01-02 15:29:32 +0530

    to check whether the string is pallindrome

--- pallindrome_checker.py
+++ pallindrome_checker.py
@@ -1,7 +1,7 @@
 #function defination 
 def is_pallindrome(s):
 
-    #removes the spaces from the string
+    #removes the spaces from the  the string
     s= s.replace(" ","").lower()
     # check if the string is equal to its reverse
     return s== s[::-1] 
